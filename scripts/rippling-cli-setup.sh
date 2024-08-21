96bf61ccbc2850666443ab0218b86cb51fd1becd
rippling-apps <apps@rippling.com>
2024-08-21 15:36:26 +0530

    Adding Rippling script

---
+++ scripts/rippling-cli-setup.sh
@@ -0,0 +1,8 @@
+
+          #!/bin/bash
+          # This script is used to setup the Rippling CLI in the Codespace
+          # It is run as part of the postCreateCommand in the devcontainer.json file
+          # It is used to install the Rippling CLI and configure it for the current user
+          # It is also used to install the required dependencies for the app
+          pip install requirements.txt
+          
